679edc80200b6107719973ba08b9a44cf85b4305
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-14 16:56:33 +0100

    Add route handlers

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const shortUrl = require("./models/shortUrls");
 
 mongoose.connect("mongodb://localhost/urlShortner", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
 
+app.use(express.urlencoded({ extended: false }));
+
 // Set view engine
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.get("/", async (req, res) => {
+	const shortUrls = await shortUrl.find();
+	res.render("index", { shortUrls: shortUrls });
 });
 
-app.post("/shortUrls", (req, res) => {});
+app.post("/shortUrls", async (req, res) => {
+	await shortUrl.create({
+		fullUrl: req.body.fullURL,
+	});
+	res.redirect("/");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}...`));
